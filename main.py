0793b1d9d79161fbdbb614628bfccd670f335dbe
Calvin Nguyen <calvin2nguyen@gmail.com>
2022-11-05 02:09:52 -0700

    no duplicates can enter playlist

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import json
 playlist_id = "2LjXB8BWlV4mTktJAGhIhM"
-CLIENT_ID = "ac0e5b1c8c5740eb94eb2ac65433be34"
-CLIENT_SECRET = "83a24f0ffa044d48ab811c99deb714dd"
+CLIENT_ID = "ef343b8adee34ec8b498ad31f12cf0cb"
+CLIENT_SECRET = "c31210f5fed44ad2ab2e9cde0994d0c9"
 REDIRECTED_URI = "http://127.0.0.1:8080/"
-
-scope = "playlist-modify-private"
+testing = "6rAmRxPEFY6XeFQjY218fz"
+scope = "playlist-modify-private user-library-modify user-library-read"
 username= "calvin2nguyen"
 new_music_kpop = "37i9dQZF1DXe5W6diBL5N4"
 rnb_kpop = "37i9dQZF1DX089MWxS7QW5"
@@ -14,41 +14,77 @@ token =SpotifyOAuth(scope=scope,username=username,client_id=CLIENT_ID,client_sec
 sp = spotipy.Spotify(auth_manager=token)
 
 "spotify:album:6nT2VfGN07ar1vdZyJY6ox"
-tracks = []
+new_tracks = []
+liked_tracks = []
+current_tracks  = []
 blackpink ="41MozSoPIsD1dJM0CLPjZF"
 
 
-def addTracks():
 
-    searched_songs = sp.search(q="album:k-pop",type="artist",limit=2,market="US")
-    
-
-    formated_list =(json.dumps(searched_songs,sort_keys=4,indent=4))
-    # print(formated_list)
-    track_uri = searched_songs["tracks"]["items"]
-    for i in range(len(track_uri)):
-        tracks.append(track_uri[i]["uri"])
-        
-    sp.playlist_add_items(playlist_id=playlist_id,items=tracks)
-
+## Get existing playlist and add 
 def grabUri(picked_playlist):
      results = sp.user_playlist_tracks(user=username,playlist_id=picked_playlist,fields="items")
      for i in range(len(results["items"])):
-        tracks.append(results["items"][i]["track"]["uri"])
+        new_tracks.append(results["items"][i]["track"]["uri"])
         
 
      
-        
-         
+    
+def new_music_added():
+    grabUri(testing)
+    sp.playlist_add_items(playlist_id=playlist_id,items=new_tracks)
+    
+
      
-def addtoPlaylist():
-    grabUri(rnb_kpop)
-    grabUri(new_music_kpop)
- 
+
+
+def update_myplaylist():
+    
+    try:
+        saved_tracks = sp.current_user_saved_tracks()
+        for i in range(len(saved_tracks["items"])):
+            liked_tracks.append(saved_tracks["items"][i]["track"]["uri"])
+        
+        current_playlist =sp.user_playlist_tracks(user=username,playlist_id=playlist_id)
+        for i in range(len(current_playlist["items"])):
+            current_tracks.append(current_playlist["items"][i]["track"]["uri"])
+        ## set to remove duplicates
+        updated_tracks = set(current_tracks)
+    
+        
+        duplicates = list(set(liked_tracks).intersection(set(current_tracks)))
+       
+        # sp.user_playlist_remove_all_occurrences_of_tracks(user=username, playlist_id=playlist_id , tracks=duplicates)
+        if len(duplicates) != 0:
+            sp.current_user_saved_tracks_delete(tracks=duplicates)
+        
+        if liked_tracks is not None:
+            add_to_playlist(liked_tracks)
+      
+    except:
+        print("no liked songs")
+
+def add_to_playlist(x):
+    sp.playlist_add_items(playlist_id=playlist_id, items=x)
+   
+         
+        
+        
+    
+    
+    
+    
+grabUri(rnb_kpop)
+update_myplaylist()
+
+
+    
+    
+
  
     
-addtoPlaylist()
-print(tracks)
+
+
 
 
     
